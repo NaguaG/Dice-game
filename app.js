fe231021733cf5e90d16f687bb6eeea893cc5d66
Nagua <naranguag@gmail.com>
2021-04-21 12:23:24 +0800

    Projectiin ehnii huvisagchiig oruulah

---
+++ app.js
@@ -0,0 +1,19 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, 1дүгээр тоглогч = 0, 2дугаар тоглогч = 1
+var activePlayer = 1;
+// Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+//Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр хадгалж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+// <div class="player-score" id="score-0">43</div>
+// document.querySelector('#score-0').textContent = dice;
+
+// document.querySelector('#score-1').innerHTML = '<em>Yes<em>';
+window.document.querySelector('#score-0').textContent = 0;
+window.document.querySelector('#score-1').textContent = 0;
+window.document.querySelector('#current-0').textContent = 0;
+window.document.querySelector('#current-1').textContent = 0;
+document.querySelector(".dice").style.display = 'none';
+console.log('Шоо : ' + dice);
