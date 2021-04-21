cfa1bd22a2066c86e265164cc428be32f44d3508
Nagua <naranguag@gmail.com>
2021-04-21 19:51:45 +0800

    Onoog uurchilj toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, 1дүгээр тоглогч = 0, 2дугаар тоглогч = 1
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 
@@ -21,9 +21,40 @@ document.getElementById('current-1').textContent = '0';
 var diceDom = document.querySelector(".dice"); 
 diceDom.style.display = 'none';
 
+// шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function (){
+
+    // 1-6 хүртэлх тооноос санамсаргүй нэг тоог гаргаж авна.
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    // Шооны зургыг вэб дээр гаргаж харуулна.
    diceDom.style.display = 'block';
+
+   // Буусан тооны шоонын зургыг вэб дээр гаргана.
    diceDom.src = 'dice-' + diceNumber +'.png';
 
+   // Буусан тоо нь 1-с ялгаатай бол идэвхитэй тоглогчийн оноог нэмэгдүүлнэ
+
+   if (diceNumber !== 1) {
+// 1-с ялгаатай тоо буувал буусан тоог тоглогчийн оноо дээр нэмнэ
+       roundScore = roundScore + diceNumber;
+document.getElementById('current-' + activePlayer).textContent = roundScore;}
+   else {
+        // 1 буувал тоглогчийн ээлжийг нөгөө тоглогч болгож солино
+        // Энэ тоглогчид 1 буусан тул оноог 0 болгох
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        roundScore = 0;
+        // Хэрэв идэвхитэй тоглогч нь 0 байвал идэвхитэй тоглогчийг 1 болгоно
+        // Үгүй бол идэвхитэй тоглогчийг 0 болгоно.
+       activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0); 
+       // Улаан цэгийг шилжүүлэх кодыг хийнэ
+       document.querySelector('.player-0-panel').classList.toggle('active');
+       document.querySelector('.player-1-panel').classList.toggle('active');
+       // Шоог түр алга болгоно.
+       diceDom.style.display = 'none';
+    //    if (activePlayer === 0) {
+    //        activePlayer = 1;
+    //     }else (activePlayer = 0);
+     
+   }
 });
