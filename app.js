72a84b8bd4e2685dac0ea97633ad0891651cab76
Nagua <naranguag@gmail.com>
2021-04-21 12:58:57 +0800

    Shoog shidehed randomoor delgetsed shoonii utga haragdahaar tohiruulah

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр хадгалж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 // <div class="player-score" id="score-0">43</div>
 // document.querySelector('#score-0').textContent = dice;
 
 // document.querySelector('#score-1').innerHTML = '<em>Yes<em>';
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
-document.querySelector(".dice").style.display = 'none';
-console.log('Шоо : ' + dice);
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+var diceDom = document.querySelector(".dice"); 
+diceDom.style.display = 'none';
+
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+   diceDom.style.display = 'block';
+   diceDom.src = 'dice-' + diceNumber +'.png';
+
+});
